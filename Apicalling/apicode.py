02a1cb5318b4d795935482e27de85257fb9f923e
Harsh-code-dev <hk070319@gmail.com>
2025-12-01 22:16:26 +0530

    Add data by api calling

--- Apicalling/apicode.py
+++ Apicalling/apicode.py
@@ -5,8 +5,8 @@ data = json.load(open("activity.json"))
 
 with open("messages_api.csv", "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
-    writer.writerow(["message"])
+    writer.writerow(["activity"])
     for item in data:
-        writer.writerow([item["message"]])
+        writer.writerow([item["activity"]])
 
 print("Saved API data")
